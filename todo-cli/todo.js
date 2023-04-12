b52d20c5d0bfb33970281a48a8c0ed53f9343b03
sushmitha-006 <118905416+sushmitha-006@users.noreply.github.com>
2023-04-12 23:03:54 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -74,22 +74,4 @@ const todoList = () => {
   todos.add({ title: 'File taxes', dueDate: tomorrow, completed: false })
   todos.add({ title: 'Pay electric bill', dueDate: tomorrow, completed: false })
   
-  console.log("My Todo-list\n")
-  
-  console.log("Overdue")
-  var overdues = todos.overdue()
-  var formattedOverdues = todos.toDisplayableList(overdues)
-  console.log(formattedOverdues)
-  console.log("\n")
-  
-  console.log("Due Today")
-  let itemsDueToday = todos.dueToday()
-  let formattedItemsDueToday = todos.toDisplayableList(itemsDueToday)
-  console.log(formattedItemsDueToday)
-  console.log("\n")
-  
-  console.log("Due Later")
-  let itemsDueLater = todos.dueLater()
-  let formattedItemsDueLater = todos.toDisplayableList(itemsDueLater)
-  console.log(formattedItemsDueLater)
-  console.log("\n\n")
+  module.exports=todoList;
